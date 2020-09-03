583785d5f5591ef540ccc69f0096ae42b40c4832
BajaJustin <justindabaja@gmail.com>
2020-09-03 19:25:33 -0400

    addded button color when pressed

--- drum-machine/src/components/drumPad.js
+++ drum-machine/src/components/drumPad.js
@@ -2,11 +2,25 @@ import React from 'react';
 
 // CSS Import
 import '../css/drumPad.css';
+
+const activeStyle = {
+  backgroundColor: '#E9C46A',
+  boxShadow: "0 5px #666",
+  height: 70
+}
+
+const inactiveStyle = {
+  backgroundColor: '#CCCCCC'
+}
 class DrumPad extends React.Component {
   constructor(props){
     super(props);
+    this.state = {
+      padStyle: inactiveStyle
+    }
     this.playAudio = this.playAudio.bind(this);
     this.handleEvent = this.handleEvent.bind(this);
+    this.activatePad = this.activatePad.bind(this);
   }
   componentDidMount() {
     document.addEventListener('keydown', this.handleEvent);
@@ -19,9 +33,20 @@ class DrumPad extends React.Component {
       this.playAudio();
     }
   }
+  activatePad() {
+      this.state.padStyle.backgroundColor === '#E9C46A' ?
+        this.setState({
+          padStyle: inactiveStyle
+        }) :
+        this.setState({
+          padStyle: activeStyle
+        });
+  }
   playAudio() {
     const audio = document.getElementById(this.props.keyTrigger);
     const innerText = document.getElementById("notePlayed");
+    this.activatePad();
+    setTimeout(() => this.activatePad(), 100);
     innerText.innerHTML = this.props.id;
     audio.currentTime = 0;
     audio.play();
@@ -29,7 +54,8 @@ class DrumPad extends React.Component {
   render() {
     return (
       <div className="drum-pad" id={this.props.id}>
-        <button onClick={this.playAudio}>
+        <button onClick={this.playAudio}
+                style={this.state.padStyle}>
           <audio id={this.props.keyTrigger}>
             <source 
               src={this.props.beat} 
